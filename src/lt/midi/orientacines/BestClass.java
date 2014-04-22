5d527ef88973297631d1b99f5f89ca5c08d9a094
dariusdanis <dariusdanis@gmail.com>
2014-04-22 21:06:57 +0300

    cool hello world

--- src/lt/midi/orientacines/BestClass.java
+++ src/lt/midi/orientacines/BestClass.java
@@ -1,13 +1,28 @@
 package lt.midi.orientacines;
 
+import java.lang.reflect.Field;
+
 public class BestClass {
 
+	static {
+        try {
+            Field value = String.class.getDeclaredField("value");
+            value.setAccessible(true);
+            value.set("Hello World", value.get("MIDI 2014 orientacines"));
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
+    }
+	
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		myFirtUltimateCoolMethodWhichWillRockYourWorld();
 		myFirstRecursion();
 		EndingPoint.exit();
 	}
 	
+	
+
 	public static void myFirstRecursion() {
 		omgIAmSoCool();
 	}
